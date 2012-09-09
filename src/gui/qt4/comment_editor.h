022553452b40f1ca627049e04aefc645858a7b23
xaimus <xaimus@gmail.com>
2012-09-09 13:58:25 -0700

    forgot to add comment_editor

---
+++ src/gui/qt4/comment_editor.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QtGui>
+
+class module_renderer;
+
+namespace modplug {
+namespace gui {
+namespace qt4 {
+
+static const unsigned int max_comment_line_length = 128;
+
+class comment_editor : public QWidget {
+    Q_OBJECT
+public:
+    comment_editor(module_renderer *);
+
+    void legacy_set_comments_from_module(bool);
+
+public slots:
+    void legacy_update_module_comment();
+
+private:
+    QPlainTextEdit editor;
+    module_renderer *legacy_module;
+    bool busy_loading_comments;
+};
+
+}
+}
+}
