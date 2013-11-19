65378765689a76c9dc7fbce07b2ab100f06b5a61
Imran Hameed <idhameed@gmail.com>
2013-11-18 22:15:13 -0800

    fix on clang/gcc

--- src/pervasives/bitset.hpp
+++ src/pervasives/bitset.hpp
@@ -7,7 +7,7 @@ namespace pervasives {
 
 template <typename Ty>
 struct bitset {
-    typedef typename Ty flag_type;
+    typedef Ty flag_type;
     typedef typename std::underlying_type<Ty>::type underlying_type;
     underlying_type internal_val;
 
@@ -18,21 +18,21 @@ struct bitset {
 template <typename Ty>
 bitset<Ty>
 bitset_set(const bitset<Ty> set, const Ty val) {
-    auto rawval = static_cast<std::underlying_type<Ty>::type>(val);
+    auto rawval = static_cast<typename bitset<Ty>::underlying_type>(val);
     return bitset<Ty>(set.internal_val | rawval);
 }
 
 template <typename Ty>
 bitset<Ty>
 bitset_unset(const bitset<Ty> set, const Ty val) {
-    auto rawval = static_cast<std::underlying_type<Ty>::type>(val);
+    auto rawval = static_cast<typename bitset<Ty>::underlying_type>(val);
     return bitset<Ty>(set.internal_val & ~rawval);
 }
 
 template <typename Ty>
 bool
 bitset_is_set(const bitset<Ty> set, const Ty val) {
-    auto rawval = static_cast<std::underlying_type<Ty>::type>(val);
+    auto rawval = static_cast<typename bitset<Ty>::underlying_type>(val);
     return (set.internal_val & rawval) != 0;
 }
 
